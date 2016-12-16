b2b93189a64a2e5382ad0f9470a37b99e6798181
Saad <saad.kalim@ca.com>
2016-12-16 11:33:17 -0800

    New search bar look.

--- app/src/main/java/saad/com/instaview/SearchActivity.java
+++ app/src/main/java/saad/com/instaview/SearchActivity.java
@@ -1,10 +1,18 @@
 package saad.com.instaview;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.OnApplyWindowInsetsListener;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.WindowInsetsCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import org.jinstagram.auth.model.Token;
 
@@ -12,39 +20,91 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import skalim.github.io.saadsandroid.util.SaadKeyboardUtil;
 
 public class SearchActivity extends AppCompatActivity {
 
     @BindView(R.id.search_activity_search_edit_text)
-    EditText editTextSearch;
+    SearchView searchView;
 
     @BindView(R.id.swiping_view)
     SwipingImagesView swipingImagesView;
+
+    @BindView(R.id.activity_search)
+    View rootView;
+
+    @BindView(R.id.search_activity_search_blur_view)
+    View blurView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
+        ComponentName cn = new ComponentName(this, SearchActivity.class);
+        searchView.setSearchableInfo(((SearchManager) getSystemService(Context.SEARCH_SERVICE)).getSearchableInfo(cn));
+        searchView.setIconifiedByDefault(false);
+        searchView.setSubmitButtonEnabled(true);
+
+
+        // Set margin for search field to allow for status bar
+        ViewCompat.setOnApplyWindowInsetsListener(searchView, new OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(View v, WindowInsetsCompat insets) {
+                final int statusBar = insets.getSystemWindowInsetTop();
+                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) searchView.getLayoutParams();
+                layoutParams.setMargins(0, statusBar, 0, 0);
+                searchView.setLayoutParams(layoutParams);
+                return insets;
+            }
+        });
+
+        handleIntent(getIntent());
 
         Intent intent = getIntent();
-        if(  intent != null ){
+        if (intent != null) {
             Uri uri = intent.getData();
-            if( uri != null ){
+            if (uri != null) {
                 String accessToken = uri.toString();
-                if( accessToken != null ){
+                if (accessToken != null) {
                     accessToken = accessToken.replace(InstaViewConsts.REDIRECT_URI + "#access_token=", "");
                     MyInstagram.init(new Token(accessToken, ""));
                 }
             }
         }
+
+        String[] urlArray = getResources().getStringArray(R.array.background_images);
+        ArrayList<String> paths = new ArrayList<>();
+        for (String url : urlArray) {
+            paths.add(url);
+        }
+        swipingImagesView.addImagePaths(paths);
+
     }
 
-    @OnClick(R.id.search_activity_button_search)
-    public void onSearch(){
-        String tag = editTextSearch.getText().toString();
-        FetchPicturesTask fetchPicturesTask = new FetchPicturesTask(this);
-        fetchPicturesTask.execute(tag);
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            onSearch(query);
+        }
+    }
+
+    public void onSearch(String query) {
+        SaadKeyboardUtil.hideKeyboard(this);
+        String[] urlArray = getResources().getStringArray(R.array.background_images);
+        ArrayList<String> paths = new ArrayList<>();
+        for (String url : urlArray) {
+            paths.add(url);
+        }
+        swipingImagesView.addImagePaths(paths);
+
+        //FetchPicturesTask fetchPicturesTask = new FetchPicturesTask(this);
+        //fetchPicturesTask.execute(query);
     }
 }
