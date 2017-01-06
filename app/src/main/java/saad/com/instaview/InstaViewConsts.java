8e6231123fa0ab229676754cc45f42d44645bf88
Saad Kalim <skalim@uwaterloo.ca>
2017-01-05 21:29:54 -0500

    Update constants

--- app/src/main/java/saad/com/instaview/InstaViewConsts.java
+++ app/src/main/java/saad/com/instaview/InstaViewConsts.java
@@ -2,9 +2,9 @@ package saad.com.instaview;
 
 
 public class InstaViewConsts {
-    public static final String REDIRECT_URI = "https://skalim.github.io/instaview";
+    public static final String REDIRECT_URI = "redirect_uri";
     static final String AUTHORIZATION_URL = "https://api.instagram.com/oauth/authorize";
-    static final String CLIENT_ID = "6d531664f106445e9a4af197073f99e9";
+    static final String CLIENT_ID = "client_id";
     private static final String CLIENT_SECRET = "client_secret";
     static final String SCOPE = "public_content";
     public static String TAG = "InstaView";
